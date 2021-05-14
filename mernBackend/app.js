7d44fec5874d232e2ff344d0e80f15e039dd3171
shubham <shubhamdigole@gmail.com>
2021-05-14 22:15:40 +0530

    master fixed dir issue

--- mernBackend/app.js
+++ mernBackend/app.js
@@ -3,15 +3,17 @@ const path  = require("path");
 const app = express();
 require("./src/db/conn");
 const port = process.env.PORT  || 3000;
+app.use(express.static(path.join(__dirname, "public")));
 
-const static_path = path.join(__dirname, "../public");
-app.use(express.static(static_path));
 
-// console.log(path.join(__dirname, "../public"));
-
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
 res.send("Hello From App Folder");
 })
+
+app.get("/", (req ,res) => {
+	res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 app.listen(port, () =>{
     console.log(`Server is running at port no ${port}`);
 })
